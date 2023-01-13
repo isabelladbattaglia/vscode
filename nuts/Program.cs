1a1162edc85660afdfba75964e3f0bb780674f87
isabelladbattaglia <38194231+isabelladbattaglia@users.noreply.github.com>
2023-01-13 01:47:21 +0000

    Full Name Methods (That's Bananas o.o)

---
+++ nuts/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static string MyMethod(string fname, string lname)
+
+{
+    //Console.WriteLine(fname + "Michael" + "Samantha" + "Joshua" + "Isabella" + "Evan" + lname + " Battaglia" );
+    string fullName = fname + " " + lname;
+    return (fullName);
+}
+MyMethod("Michael", "Battaglia" );
+MyMethod("Samantha", "Battaglia");
+MyMethod("Joshua", "Battaglia");
+MyMethod("Kristin", "Battaglia");
+MyMethod("Isabella", "Battaglia");
+MyMethod("Evan", "Battaglia");
+
+
+Console.WriteLine(MyMethod("Michael", "Battaglia"));
+Console.WriteLine(MyMethod("Kristin", "Battaglia"));
+Console.WriteLine(MyMethod("Isabella", "Battaglia"));
+Console.WriteLine(MyMethod("Joshua", "Battaglia"));
+Console.WriteLine(MyMethod("Samantha", "Battaglia"));
+Console.WriteLine(MyMethod("Evan", "Battaglia"));
